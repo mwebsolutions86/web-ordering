cf0e98792e94e1a986c56332ab4dae9855c72fe8
mwebsolutions86 <mazouz86mohcine@hotmail.com>
2025-12-20 17:36:22 +0100

    Description de ce que tu as changé

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+const nextConfig = {
+  // Vos options de configuration vont ici
+  reactStrictMode: true,
+  images: {
+    domains: ['ton-domaine.com'], // Ajoutez les domaines d'images si nécessaire
+  },
+};
+
+export default nextConfig;
